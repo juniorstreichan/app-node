f7e1f2e70b96fe2cb06b84b59bb8d90ddecc40ea
Junior Erdmann <junior.erdmann@outlook.com>
2018-08-14 15:21:50 -0400

    Inicio dos models e configuração do MongoDB/ Product

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const router = express.Router();
+
+mongoose.connect('mongodb://.mlab.com:19442/produtos?authSource=produtos&w=1')
+//#region CARREGAMENTO DOS MODELS
+const Product = require('./models/product');
+ //#endregion
+
 
 //#region CARREGAMENTO DAS ROTAS
 const indexRoute = require("./routes/index.route");
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,31 @@
 "use strict";
 
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+exports.get = (request, response, next) => {
+  Product.find({ active: true }, "title price slug tags")
+    .then(result => {
+      response.status(200).send(result);
+    })
+    .catch(err => {
+      response.status(400).send({ message: "Erro", data: err });
+    });
+};
+
 exports.post = (request, response, next) => {
-  response.status(201).send(request.body);
+  let product = new Product(request.body);
+  product
+    .save()
+    .then(result => {
+      response
+        .status(201)
+        .send({ message: "Cadastrado com sucesso", data: product });
+    })
+    .catch(err => {
+      response.status(400).send({ message: "Falha no cadastro", data: err });
+    });
 };
 
 exports.put = (request, response, next) => {
